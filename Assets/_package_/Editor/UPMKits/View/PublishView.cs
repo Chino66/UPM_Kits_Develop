6247ffcad43138b2ff40078eaeb4d9f7d2d623fd
Chino66 <447660276@qq.com>
2022-03-23 00:08:21 +0800

    Update PublishView.cs

--- Assets/_package_/Editor/UPMKits/View/PublishView.cs
+++ Assets/_package_/Editor/UPMKits/View/PublishView.cs
@@ -1,10 +1,12 @@
+using System;
+using System.Diagnostics;
 using System.IO;
 using CommandTool;
 using UIElementsKits;
 using UIElementsKits.UIFramework;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.UIElements;
+using Debug = UnityEngine.Debug;
 
 namespace UPMKits
 {
@@ -23,7 +25,9 @@ namespace UPMKits
             var btn = _cache.Get<Button>("publish_btn");
             btn.clicked += () =>
             {
-                System.Diagnostics.Process.Start("npm", @"publish G:\Github\UPM_Kits_Develop\Assets\_package_");
+                // run();
+                ExecuteCommand("");
+                // System.Diagnostics.Process.Start("npm", @"publish G:\Github\UPM_Kits_Develop\Assets\_package_");
                 // Command.RunAsync("npm", ctx =>
                 // {
                 //     foreach (var message in ctx.Messages)
@@ -46,6 +50,32 @@ namespace UPMKits
 
         public void Refresh()
         {
+        }
+
+        private void ExecuteCommand(string command)
+        {
+            var processInfo =
+                new ProcessStartInfo("npm.cmd", @"publish G:\Github\UPM_Kits_Develop\Assets\_package_ & exit");
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+            {
+                Debug.Log("output>>" + e.Data);
+            };
+            process.BeginOutputReadLine();
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => { Debug.Log("error>>" + e.Data); };
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            Debug.Log($"ExitCode: {process.ExitCode}");
+            process.Close();
         }
     }
 }
